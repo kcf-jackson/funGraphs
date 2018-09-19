36110162cd833a20d707e035c9d148236fb3f020
kcf-jackson <kcf.jackson@gmail.com>
2018-09-19 16:41:32 +1000

    Minor refactoring

--- R/extract.R
+++ R/extract.R
@@ -14,24 +14,32 @@ get_calls_in_file <- function(fpath) {
 
 
 # function -> calls
+# @examples
+# fun0 <- rlang::parse_expr("a <- function(x){print(x)}")
+# get_calls_in_function(fun0)
 get_calls_in_function <- function(fun0) {
+  is_fun <- . %>% { rlang::is_call(., c("<-", "=")) }
   if (!is_fun(fun0)) return(NULL)
-  list(unlist(get_all_calls(fun0[[3]]))) %>%
-    setNames(extract_fun_name(fun0))
+
+  fun_name <- as.character(fun0[[2]])
+  fun_body <- fun0[[3]]
+  get_all_calls(fun_body) %>%
+    unlist() %>%   # remove nested structure
+    list() %>%     # add back the list structure
+    setNames(fun_name)
 }
 
 
 # Call -> all calls within
 # @note It doesn't capture calls that appear in the function formal arguments
 # @examples
-# tmp <- rlang::parse_expr("a <- function(b, d) {print(b); print(d)}")
-# get_all_calls(tmp)
+# fun0 <- rlang::parse_expr("a <- function(b, d) {print(b); print(d)}")
+# get_all_calls(fun0)
 get_all_calls <- function(call0) {
   if (rlang::is_call(call0)) {
-    return(list(
-      as.character(call0[[1]]),
-      purrr::map(as.list(call0[-1]), get_all_calls)
-    ))
+    fun_name <- as.character(call0[[1]])
+    fun_rest <- as.list(call0[-1])
+    return(list(fun_name, purrr::map(fun_rest, get_all_calls)))
   }
   # Include potential functions appeared as arguments
   if (is.null(call0) || is.list(call0)) return(NULL)
